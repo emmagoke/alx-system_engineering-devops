71617aecb9d2c9ccb94eed4babf5d299b7767dba
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-04-21 15:46:49 -0700

    Added question 5

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,26 @@
+# This puppet scripts installs Nginx and configures it
+# This script also handles 301 redirect when querying /redirect_me.
+
+package {'Nginx':
+  ensure   => present,
+  name     => 'nginx',
+  provider => apt
+}
+
+file {'index':
+  ensure  => present,
+  path    => '/var/www/html/index.nginx-debian.html',
+  content => 'Hello World!'
+}
+
+file_line {'redirect_me':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen [::]:80',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;'
+}
+
+service {'Nginx-start':
+  ensure => running,
+  name   => 'nginx'
+}
