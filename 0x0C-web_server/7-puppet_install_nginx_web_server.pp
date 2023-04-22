f31afe88ed5ec3aee4c1ddf53ba061d2b200c6bb
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-04-21 18:26:52 -0700

    Added question 5

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,12 +1,13 @@
+include stdlib
 # This puppet scripts installs Nginx and configures it
 # This script also handles 301 redirect when querying /redirect_me.
-include stdlib
+
 # stdlib allows me to use file_line
 
 package {'Nginx':
   ensure   => present,
   name     => 'nginx',
-  provider => apt,
+  provider => 'apt',
 }
 
 file {'index':
@@ -19,10 +20,10 @@ file_line {'redirect_me':
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen [::]:80',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;'
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
 service {'Nginx-start':
-  ensure => running,
-  name   => 'nginx'
+  ensure  => running,
+  name    => 'nginx',
 }
