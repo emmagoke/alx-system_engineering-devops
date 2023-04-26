9bc10514ce7fe07057fccccc963a7f28e1b71288
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-04-26 01:44:01 -0700

    Added question 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This python script Records all tasks that are owned by this employee
+using the format "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+"""
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    user_id = int(sys.argv[1])
+    user_data_url = 'https://jsonplaceholder.typicode.com/users'
+    user_todo_url = 'https://jsonplaceholder.typicode.com/todos'
+    user_data = requests.get(user_data_url).json()
+    user_tasks = requests.get(user_todo_url).json()
+    user_name = ''
+
+    for user in user_data:
+        if user['id'] == user_id:
+            user_name = user['name']
+
+    with open("{}.csv".format(user_id), 'w') as file_:
+        for task in user_tasks:
+            if task['userId'] == user_id:
+                write_ = '"{}", "{}", "{}", "{}"\n'.format(
+                         user_id, user_name, task['completed'], task['title'])
+                file_.write(write_)
