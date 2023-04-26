469fd613149ef2fd8e46a15d3099b46dcae21d57
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-04-26 07:56:21 -0700

    Added question 3

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+This python script Records all tasks that are owned by this employee
+Using this format { "USER_ID": [{"task": "TASK_TITLE",
+"completed": TASK_COMPLETED_STATUS, "username": "USERNAME"},
+ {"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS,
+ "username": "USERNAME"}, ... ]}
+"""
+if __name__ == '__main__':
+    import json
+    import requests
+    import sys
+
+    user_id = int(sys.argv[1])
+    api_url = 'https://jsonplaceholder.typicode.com/'
+    user_data = requests.get(api_url + "users/{}".format(user_id)).json()
+    user_todos = requests.get(api_url + 'todos').json()
+
+    user_name = user_data['username']
+    user_tasks = []
+    each = {}
+
+    i = 0
+    for task in user_todos:
+        if task['userId'] == user_id:
+            #print(each)
+            each["task"] = "{}".format(task.get('title'))
+            each["completed"] = "{}".format(task.get('completed'))
+            each["username"] = "{}".format(user_name)
+            user_tasks.append(each.copy())
+
+    user_json = {"{}".format(user_id) : user_tasks}
+    
+    with open("{}.json".format(user_id), 'w') as file_:
+        json.dump(user_json, file_)
