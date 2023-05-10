0faa6db3a5ce7c75f2767b5d23c26437d94e757a
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-05-09 20:26:13 -0700

    Added task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This python script returns the number of subscribers (not active users, total
+subscribers) for a given subreddit.
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    returns the number of subscribers of the subreddit
+    """
+    url = "https://www.reddit.com/r/"
+    headers = {'User-Agent': 'MyBot/0.0.1'}
+    data = requests.get("{}{}/about.json".format(url, subreddit),
+                        headers=headers)
+    if data:
+        data = data.json()
+        no_sub = data['data']['subscribers']
+        return no_sub
+    return 0
