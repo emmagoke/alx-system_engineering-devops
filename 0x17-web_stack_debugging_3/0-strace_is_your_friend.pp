a6cb44a936a9b517063e8525c608205547da1b3f
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-05-10 22:01:18 -0700

    Added task 1

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -3,5 +3,5 @@
 exec {'fix-wordpress':
   command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   provider => 'shell',
-  path     => '/usr/bin/',
+  path     => '/usr/local/bin/:/bin/',
 }
