bf72cdae5bdf65dcdc7ada4a7756e5095ca366b3
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-05-19 06:01:03 -0700

    Made changes

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -10,18 +10,31 @@ package {'Nginx':
   provider => 'apt',
 }
 
+include nginx
+
+class { 'nginx':
+  manage_repo    => true,
+  package_source => 'nginx-stable',
+}
+
+ngnx::resource::server { '35.175.135.243':
+  listen_port      => 80,
+  www_root         => '/var/www/html',
+  vhost_cfg_append => { 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;' }
+}
+
 file {'index':
   ensure  => present,
   path    => '/var/www/html/index.nginx-debian.html',
   content => 'Hello World!',
 }
 
-file_line {'redirect_me':
-  ensure => present,
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen [::]:80',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-}
+# file_line {'redirect_me':
+#  ensure => present,
+#  path   => '/etc/nginx/sites-available/default',
+#  after  => 'listen [::]:80',
+#  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+#} 
 
 service {'Nginx-start':
   ensure     => running,
