5be544a2182543ad065247687c99ed3cd8c78348
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-05-20 16:52:24 -0700

    Added task 1

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+include stdlib
+# This script increase the worker processes of nginx
+# so as to able to handle more request
+# Changes the ULIMIT in /etc/default/nginx from 15 to 4096
+
+exec { 'ulimit to 4096':
+  path    => '/bin',
+  command => "sed -i 's/15/4096/g' /etc/default/nginx",
+}
+
+exec { 'restart nginx' :
+  command => '/usr/bin/service nginx restart',
+}
