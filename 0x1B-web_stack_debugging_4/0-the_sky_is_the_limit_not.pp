39f2d8d53d24cc6e5a00da408202fdcc8ca95402
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-05-20 17:18:47 -0700

    Added task 1 after some corrections

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,11 +2,7 @@
 # so as to able to handle more request
 # Changes the ULIMIT in /etc/default/nginx from 15 to 4096
 
-exec { 'ulimit to 4096':
-  path    => '/bin',
-  command => "sed -i 's/15/4096/' /etc/default/nginx",
-}
-
-exec { 'restart nginx' :
-  command => '/usr/sbin/service nginx restart',
+exec { 'set ulimit to 4096 and restart nginx':
+  command  => "sed -i 's/15/4096/' /etc/default/nginx; service nginx reatart",
+  provider => shell,
 }
